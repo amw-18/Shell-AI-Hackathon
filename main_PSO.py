ada26fde5451bb63cc87f82544e540f9cb4bcabc
Advait Walsangikar <awalsangikar@gmail.com>
2020-10-02 18:34:53 +0530

    added an optimal swarm of 64 particles as csv and produced one optimal swarm solution

--- main_PSO.py
+++ main_PSO.py
@@ -1,73 +1,75 @@
 from PSO.pso_modified_wind import *
 import numpy as np
 import pandas as pd
-n_turbs = 50
 
-# setting turbine radius
-turb_rad = 50.0
+# if __name__ == "__main__":
+#     n_turbs = 50
 
-# Loading the power curve
-power_curve   =  loadPowerCurve('./Shell_Hackathon Dataset/power_curve.csv')
+#     # setting turbine radius
+#     turb_rad = 50.0
 
-# Loading wind data 
-years = ['07']
-wind_inst_freqs = []
-for y in years:
-    wind_inst_freqs.append(binWindResourceData(f'./Shell_Hackathon Dataset/Wind Data/wind_data_20{y}.csv'))
+#     # Loading the power curve
+#     power_curve   =  loadPowerCurve('./Shell_Hackathon Dataset/power_curve.csv')
 
-
-# preprocessing the wind data to avoid repeated calculations
-n_wind_instances, cos_dir, sin_dir, wind_sped_stacked, C_t = preProcessing(power_curve,n_turbs)
-
-kwargs = {'turb_rad': turb_rad, 'power_curve': power_curve, 'wind_inst_fs': wind_inst_freqs,
-        'n_wind_instances': n_wind_instances, 'cos_dir': cos_dir, 'sin_dir': sin_dir, 'wind_sped_stacked': wind_sped_stacked,
-        'C_t': C_t} 
+#     # Loading wind data 
+#     years = ['07']
+#     wind_inst_freqs = []
+#     for y in years:
+#         wind_inst_freqs.append(binWindResourceData(f'./Shell_Hackathon Dataset/Wind Data/wind_data_20{y}.csv'))
 
 
-# getting ideal_AEP for normalization purpose
-ref_loc = get_random_arrangement(1)
-ideal_AEP = 0
-for wind_inst_freq in wind_inst_freqs:
-    ideal_AEP += getAEP(turb_rad, ref_loc, power_curve, wind_inst_freq, *preProcessing(power_curve, 1))
+#     # preprocessing the wind data to avoid repeated calculations
+#     n_wind_instances, cos_dir, sin_dir, wind_sped_stacked, C_t = preProcessing(power_curve,n_turbs)
 
-ideal_AEP /= len(wind_inst_freqs)
-ideal_AEP *= n_turbs
+#     kwargs = {'turb_rad': turb_rad, 'power_curve': power_curve, 'wind_inst_fs': wind_inst_freqs,
+#             'n_wind_instances': n_wind_instances, 'cos_dir': cos_dir, 'sin_dir': sin_dir, 'wind_sped_stacked': wind_sped_stacked,
+#             'C_t': C_t} 
 
-# defining parameters for optimization
-a = 100  # weight for the proximity penalty -- critical only if random initialization done
-c2 = 1  # social
-w = 0  # inertia
-c1 = 0 # cognitive
 
-kwargs['n_turbs'] = n_turbs
-kwargs['a'] = a
-kwargs['ideal_AEP'] = ideal_AEP
+#     # getting ideal_AEP for normalization purpose
+#     ref_loc = get_random_arrangement(1)
+#     ideal_AEP = 0
+#     for wind_inst_freq in wind_inst_freqs:
+#         ideal_AEP += getAEP(turb_rad, ref_loc, power_curve, wind_inst_freq, *preProcessing(power_curve, 1))
 
-opt_swarm = []
-best_score = 0
-while len(opt_swarm) < 64:
-    optimizer = my_optim(n_turbs, a, c1, c2, w, get_init(n_turbs, 64))
+#     ideal_AEP /= len(wind_inst_freqs)
+#     ideal_AEP *= n_turbs
 
-    cost,pos = optimizer.optimize(obj, iters=200, kwargs=kwargs, verbose=False)
+#     # defining parameters for optimization
+#     a = 100  # weight for the proximity penalty -- critical only if random initialization done
+#     c2 = 1  # social
+#     w = 0  # inertia
+#     c1 = 0 # cognitive
 
-    AEP = -kwargs['ideal_AEP']*obj_util(pos, **kwargs)
-    best_score = max(best_score, AEP)
-    if AEP > 517.30:
-        opt_swarm.append(pos)
-        print('*', end='')
-print()
+#     kwargs['n_turbs'] = n_turbs
+#     kwargs['a'] = a
+#     kwargs['ideal_AEP'] = ideal_AEP
 
-opt_swarm = np.array(opt_swarm)
-optimizer = my_optim(n_turbs, a, c1, c2, w, opt_swarm)
-cost, pos = optimizer.optimize(obj, iters=200, kwargs=kwargs)
-AEP = -kwargs['ideal_AEP']*obj_util(pos, **kwargs)
+#     opt_swarm = []
+#     best_score = 0
+#     while len(opt_swarm) < 64:
+#         optimizer = my_optim(n_turbs, a, c1, c2, w, get_init(n_turbs, 64))
 
-print('random_swarm aep', best_score)
-print('opt_swarm aep', AEP)
+#         cost,pos = optimizer.optimize(obj, iters=200, kwargs=kwargs, verbose=False, n_processes=12)
 
-oswarm = pd.Dataframe(opt_swarm)
-oswarm.to_csv("C:/Users/awals/Downloads/Shell AI Hackathon/PSO/oswarm0.csv",index=False)
+#         AEP = -kwargs['ideal_AEP']*obj_util(pos, **kwargs)
+#         best_score = max(best_score, AEP)
+#         if AEP > 517.3:
+#             opt_swarm.append(pos)
+#             print('*', end='')
+#     print()
 
-ans = np.array(pos).reshape((50, 2))
-turbines = pd.DataFrame(ans,columns=['x','y'])
-turbines.to_csv("C:/Users/awals/Downloads/Shell AI Hackathon/Trials/opt_swarm_ans0.csv",index=False)
+#     opt_swarm = np.array(opt_swarm)
+#     optimizer = my_optim(n_turbs, a, c1, 1.1, 0.01, opt_swarm)
+#     cost, pos = optimizer.optimize(obj, iters=200, kwargs=kwargs, n_processes=12)
+#     AEP = -kwargs['ideal_AEP']*obj_util(pos, **kwargs)
+
+#     print('random_swarm aep', best_score)
+#     print('opt_swarm aep', AEP)
+
+#     oswarm = pd.DataFrame(opt_swarm)
+#     oswarm.to_csv("C:/Users/awals/Downloads/Shell AI Hackathon/PSO/oswarm0.csv",index=False)
+
+#     ans = np.array(pos).reshape((50, 2))
+#     turbines = pd.DataFrame(ans,columns=['x','y'])
+#     turbines.to_csv("C:/Users/awals/Downloads/Shell AI Hackathon/Trials/opt_swarm_ans0.csv",index=False)
